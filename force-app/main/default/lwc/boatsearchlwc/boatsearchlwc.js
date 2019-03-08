48bf9014f30ef95792dcd5a3ee9681584f54d86e
gitfish <mfisher.au@gmail.com>
2019-03-08 16:55:58 +1100

    Progress.

--- force-app/main/default/lwc/boatsearchlwc/boatsearchlwc.js
+++ force-app/main/default/lwc/boatsearchlwc/boatsearchlwc.js
@@ -3,5 +3,6 @@ import { LightningElement } from 'lwc';
 export default class Boatsearchlwc extends LightningElement {
     onFormSubmit(event) {
         console.log(`-- Form Submit: ${JSON.stringify(event.detail)}`);
+        this.template.querySelector("c-boatsearchresultslwc").search(event.detail.boatTypeId);
     }
 }
---
+++ force-app/main/default/lwc/boatsearchresultslwc/boatsearchresultslwc.js
@@ -0,0 +1,23 @@
+import { LightningElement, track, api } from 'lwc';
+import getBoats from "@salesforce/apex/BoatSearchResults.getBoats";
+
+export default class Boatsearchresultslwc extends LightningElement {
+    @track
+    boats = [];
+
+    @track
+    boatsError;
+
+    get boatsEmpty() {
+        return !this.boats || this.boats.length === 0;
+    }
+
+    @api
+    async search(boatTypeId) {
+        try {
+            this.boats = await getBoats({ boatTypeId: boatTypeId });
+        } catch(error) {
+            this.boatsError = error;
+        }
+    }
+}
